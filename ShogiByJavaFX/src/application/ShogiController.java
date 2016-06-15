c9050df575f787286a7a6b468e47032ed030464a
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-06-15 19:22:28 +0900

    ImageViewのクリックイベントをテスト

--- ShogiByJavaFX/src/application/ShogiController.java
+++ ShogiByJavaFX/src/application/ShogiController.java
@@ -1,5 +1,18 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
 public class ShogiController {
-	
+	@FXML private ImageView cell_00;
+
+
+	@FXML
+	private void clickCell_00(){
+		Image ou = new Image(getClass().getResourceAsStream("sgl01.png"));
+
+		cell_00.setImage(ou);
+	}
 }
