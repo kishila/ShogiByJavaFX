74893511b82f635bbbff4dd47c39282490c0a062
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-06-15 11:45:10 +0900

    ディレクトリ構造の変更(assetsフォルダ追加)

--- ShogiByJavaFX/src/application/Main.java
+++ ShogiByJavaFX/src/application/Main.java
@@ -12,11 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Shogi.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,700,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			System.out.println(root.getChildren());
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
