e0c8ea57b2eb321b864d04387339aba7132cda69
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-06-22 13:15:10 +0900

    セルのid取得

--- ShogiByJavaFX/src/application/Controllers/ShogiController.java
+++ ShogiByJavaFX/src/application/Controllers/ShogiController.java
@@ -1,18 +1,28 @@
 package application.Controllers;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 
 public class ShogiController {
 	@FXML private ImageView cell_00;
+	@FXML private ImageView cell_10;
+	@FXML private ImageView cell_20;
+	@FXML private ImageView cell_30;
+	@FXML private ImageView cell_40;
+	@FXML private ImageView cell_50;
+	@FXML private ImageView cell_60;
+	@FXML private ImageView cell_70;
+	@FXML private ImageView cell_80;
 
 	@FXML
-	private void clickCell_00(){
-		Image ou = new Image(getClass().getResourceAsStream("sgl01.png"));
-		cell_00.setImage(ou);
-		cell_00.setBlendMode(BlendMode.RED);
+	private void onClickCell(MouseEvent event){
+		String id = event.getPickResult().getIntersectedNode().getId(); // cellのidの取得
+
+		System.out.println(id);
+
+
 	}
 }
