a5c898e3337b54d0e49b6d228f75a26b04a71c28
kishila <s16g459@stu.kagawa-u.ac.jp>
2016-07-05 09:43:40 +0900

    ボードを作成

---
+++ ShogiByJavaFX/src/application/Models/component/Board.java
@@ -0,0 +1,10 @@
+package application.Models.component;
+
+import static application.Models.constant.BoardConfig.*;
+import javafx.scene.control.Cell;
+
+public class Board {
+	private Cell[][] cell = new  Cell[LENGTH][LENGTH];
+
+
+}
